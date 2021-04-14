df6d8bb8ccd7f6311fabf445ce262881018b2a13
jkim-ualbany <70613823+jkim-ualbany@users.noreply.github.com>
2021-04-14 12:08:12 -0400

    Add files via upload

---
+++ Capstone Project iOS/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Capstone Project iOS
+//
+//  Created by Harp on 3/22/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Capstone Project iOS/Models/PatientModel.swift
@@ -0,0 +1,8 @@
+//
+//  PatientModel.swift
+//  Capstone Project iOS
+//
+//  Created by Harp on 3/28/21.
+//
+
+import Foundation
